4015205f6310d03ddb9ab19c43077864f5246fd6
abhimanyulal <abhimanyu.lal@outlook.com>
2023-08-06 18:15:59 +0530

    Update ldr.ino

--- ldr.ino
+++ ldr.ino
@@ -11,10 +11,7 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(11, HIGH);
-  delay(500);
   ldr = analogRead(ldrpin);
   Serial.println(ldr);
   delay(500);
-  digitalWrite(11, LOW);
-  delay(1000);
 }
